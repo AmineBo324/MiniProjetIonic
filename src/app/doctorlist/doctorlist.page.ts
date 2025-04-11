0a93c34dc42823970055d19be3e6ac965fd2cf0a
Amine <amine.bouabid2018@gmail.com>
2025-04-11 19:45:31 +0100

    more update

--- src/app/doctorlist/doctorlist.page.ts
+++ src/app/doctorlist/doctorlist.page.ts
@@ -9,21 +9,29 @@ interface Doctor {
   nom: string;
   prenom: string;
   specialite: string;
-  email: string;  
-  zone_geographique : string,
-  rating : string,
-  image : string
+  email: string;
+  zone_geographique: string,
+  rating: string,
+  image: string
 }
 
 @Component({
   selector: 'app-doctor-list',
   templateUrl: './doctorlist.page.html',
   styleUrls: ['./doctorlist.page.scss'],
+  standalone: true,
   imports: [CommonModule, IonicModule, FormsModule]
 })
 export class DoctorlistPage implements OnInit {
   doctors: Doctor[] = [];
   filteredDoctors: Doctor[] = [];
+  
+  // Nouvelles propriétés pour la recherche unifiée
+  searchTerm: string = '';
+  searchType: string = 'specialite'; // Valeur par défaut
+  showFilters: boolean = false;
+  
+  // Maintenir les anciennes propriétés pour compatibilité
   searchSpecialite: string = '';
   searchZone: string = '';
 
@@ -34,7 +42,7 @@ export class DoctorlistPage implements OnInit {
   }
 
   fetchDoctors() {
-    this.http.get<Doctor[]>('http://localhost:5000/medecin/all')  
+    this.http.get<Doctor[]>('http://localhost:5000/medecin/all')
       .subscribe((response) => {
         this.doctors = response;
         this.filteredDoctors = response;
@@ -43,7 +51,22 @@ export class DoctorlistPage implements OnInit {
       });
   }
 
+  // Méthode pour basculer l'affichage des options de filtre
+  toggleFilterOptions() {
+    this.showFilters = !this.showFilters;
+  }
+
+  // Méthode de recherche mise à jour
   searchDoctors() {
+    // Vérifier le type de recherche sélectionné et assigner la valeur appropriée
+    if (this.searchType === 'specialite') {
+      this.searchSpecialite = this.searchTerm;
+      this.searchZone = '';
+    } else {
+      this.searchZone = this.searchTerm;
+      this.searchSpecialite = '';
+    }
+    
     let queryParams = [];
     if (this.searchSpecialite.trim()) {
       queryParams.push(`specialite=${this.searchSpecialite}`);
@@ -51,18 +74,21 @@ export class DoctorlistPage implements OnInit {
     if (this.searchZone.trim()) {
       queryParams.push(`zone_geographique=${this.searchZone}`);
     }
-
+    
     const queryString = queryParams.length ? '?' + queryParams.join('&') : '';
-
+    
     this.http.get<Doctor[]>(`http://localhost:5000/medecin/search${queryString}`)
       .subscribe((response) => {
         this.filteredDoctors = response;
       }, (error) => {
         console.error('Error searching doctors:', error);
       });
+    
+    // Masquer les options de filtre après la recherche
+    this.showFilters = false;
   }
 
   viewDoctorDetails(email: string) {
-    this.router.navigate(['/appointment', email]);  
+    this.router.navigate(['/appointment', email]);
   }
 }
